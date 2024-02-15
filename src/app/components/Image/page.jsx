818cb4a514ce37eeaa2c03d088918d0e4c5685b5
Omshree <am.omsamal@gmail.com>
2024-02-15 22:28:26 +0530

    Added Comments

--- src/app/components/Image/page.jsx
+++ src/app/components/Image/page.jsx
@@ -1,16 +1,25 @@
 "use client";
 import styles from "../../styles/Image.module.css";
 
+// ImagePopup component to display an image popup with navigation buttons
 const ImagePopup = ({ image, onClose, onNext, onPrevious }) => {
   return (
+    // Outer container for the popup, clicking on it closes the popup
     <div className={styles.image_popup} onClick={onClose}>
+      {/* Inner container for the popup content */}
       <div className={styles.popup_content} onClick={e => e.stopPropagation()}>
+        {/* Close button to close the popup */}
         <button className={styles.close_btn} onClick={onClose}>Close</button>
+        {/* Container for next and previous navigation buttons */}
         <div className={styles.next_prev}>
-        <button className={styles.prev_btn} onClick={onPrevious}>{`<`}</button>
-        <img src={image.download_url} alt={image.title} />
-        <button className={styles.next_btn} onClick={onNext}>{`>`}</button>
+          {/* Previous button */}
+          <button className={styles.prev_btn} onClick={onPrevious}>{`<`}</button>
+          {/* Image to display */}
+          <img src={image.download_url} alt={image.title} />
+          {/* Next button */}
+          <button className={styles.next_btn} onClick={onNext}>{`>`}</button>
         </div>
+        {/* Details section */}
         <div className={styles.details}>
           <p><b>Author: </b> {image.author}</p>
           <p><b>Download URL: </b><a href={image.download_url} target="_blank">Download</a></p>
--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,23 @@
 "use client"
-/* Home.js */
-
 import React, { useState, useEffect, useRef } from 'react';
 import GalleryGrid from './components/Gallery/page';
 import ImagePopup from './components/Image/page';
 import ImagePlaceholder from './components/Loader/page';
 
 export default function Home() {
-  const [images, setImages] = useState([]);
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [page, setPage] = useState(1);
-  const [fetchingPage, setFetchingPage] = useState(false);
-  const scrollPositionRef = useRef(0);
+  const [images, setImages] = useState([]); // State for storing images
+  const [selectedImage, setSelectedImage] = useState(null); // State for storing the selected image
+  const [loading, setLoading] = useState(true); // State for loading indicator
+  const [page, setPage] = useState(1); // State for pagination
+  const [fetchingPage, setFetchingPage] = useState(false); // State for fetching new page
+  const scrollPositionRef = useRef(0); // Ref for storing scroll position
 
+  // Fetch images from API when component mounts
   useEffect(() => {
     fetchImages();
   }, []);
 
+  // Add scroll event listener when loading state changes
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
     return () => {
@@ -25,6 +25,7 @@ export default function Home() {
     };
   }, [loading]);
 
+  // Fetch images from API
   const fetchImages = async () => {
     try {
       setFetchingPage(true);
@@ -41,32 +42,33 @@ export default function Home() {
     }
   }; 
 
+  // Handle click on an image
   const handleImageClick = (image) => {
     setSelectedImage(image);
   };
 
+  // Close the image popup
   const handleClosePopup = () => {
     setSelectedImage(null);
   };
 
+  // Show the next image in the popup
   const handleNextImage = () => {
-    // Implement logic to show the next image
-    // For example:
     const currentIndex = images.findIndex(img => img.id === selectedImage.id);
     if (currentIndex !== -1 && currentIndex < images.length - 1) {
       setSelectedImage(images[currentIndex + 1]);
     }
   };
 
+  // Show the previous image in the popup
   const handlePreviousImage = () => {
-    // Implement logic to show the previous image
-    // For example:
     const currentIndex = images.findIndex(img => img.id === selectedImage.id);
     if (currentIndex !== -1 && currentIndex > 0) {
       setSelectedImage(images[currentIndex - 1]);
     }
   };
 
+  // Handle scroll event to fetch more images when reaching the bottom of the page
   const handleScroll = () => {
     if (
       window.innerHeight + window.scrollY >= document.body.offsetHeight &&
@@ -77,6 +79,7 @@ export default function Home() {
     }
   };
 
+  // Restore scroll position after fetching new images
   useEffect(() => {
     if (!fetchingPage) {
       window.scrollTo(0, scrollPositionRef.current);
@@ -85,12 +88,15 @@ export default function Home() {
 
   return (
     <div className="app">
+      {/* Header */}
       <h1 className='heading fixed-header'>GalleryX</h1>
+      {/* Loading indicator or Gallery */}
       {loading ? (
         <ImagePlaceholder />
       ) : (
         <GalleryGrid images={images} onImageClick={handleImageClick} />
       )}
+      {/* Image Popup */}
       {selectedImage && (
         <ImagePopup
           image={selectedImage}
